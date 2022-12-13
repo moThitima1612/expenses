3b768fbe8f5ada77ebcd1405e93b4faaac8d566e
moThitima1612 <110826833+moThitima1612@users.noreply.github.com>
2022-12-13 14:15:03 +0700

    Add files via upload

---
+++ number.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 13, 2022 at 08:14 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `number`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `number`
+--
+
+CREATE TABLE `number` (
+  `id` int(10) NOT NULL,
+  `item_name` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `amount` float(8,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `number`
+--
+
+INSERT INTO `number` (`id`, `item_name`, `date`, `name`, `amount`) VALUES
+(1, 'ค่าประชุม', '0222-03-12', 'ฐิติมา', 150.00),
+(2, 'ค่าเดินทาง', '0000-00-00', 'ฐิติมา', 1000.00),
+(3, 'ค่าเย็ด1ครั้ง', '2565-12-10', 'เฟื่องลัดดา', 1500.00),
+(4, 'ค่าอุปกรณ์', '2022-12-12', 'ฐิติมา', 5000.00),
+(5, 'ค่าอาหาร', '2564-02-18', 'ฐิติมา', 8000.00);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `number`
+--
+ALTER TABLE `number`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `number`
+--
+ALTER TABLE `number`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
